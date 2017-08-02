86c2dde196410dc289bd566eef02437609c30b83
cav2008 <cavanlaw@hotmail.com>
2017-08-02 21:30:41 +0100

    Add babel-node and es6 for backend code

--- src/server/server.js
+++ src/server/server.js
@@ -3,9 +3,27 @@
  * var express = require('express');
  * var app = express();
  */
-var app = require('express')();
-var server = require('http').Server(app);
-var path = require('path');
+// var app = require('express')();
+// var server = require('http').Server(app);
+// var path = require('path');
 
-server.listen(8000);
-console.log('Listening at http://localhost:8000');
+// Using ES6 way instead of node
+import express from 'express';
+import http from 'http';
+import path from 'path';
+
+/**
+ * Main Server class listening to port 8000
+ */
+export default class Server {
+  constructor() {
+    const app = express();
+    const server = http.Server(app);
+
+    server.listen(8000);
+    console.log('Listening at http://localhost:8000');
+  }
+}
+
+// Make instance of Server object
+const chatApp = new Server();
