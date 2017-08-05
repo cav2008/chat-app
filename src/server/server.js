347bfa26a5b4732785a2381d0cd774c0f3e07c63
cav2008 <cavanlaw@hotmail.com>
2017-08-05 21:42:05 +0100

    Make server a static method and also a singleton

--- src/server/server.js
+++ src/server/server.js
@@ -16,14 +16,24 @@ import path from 'path';
  * Main Server class listening to port 8000
  */
 export default class Server {
-  constructor() {
-    const app = express();
-    const server = http.Server(app);
 
-    server.listen(8000);
-    console.log('Listening at http://localhost:8000');
+  /**
+   * Create only a single instance of the server. (singleton design pattern)
+   * Static method, therefore it is belong to the class and not to the instance
+   * of it. Call it straight without making instance of it.
+   * Since we only use the Server class to create a server it make more sense
+   * for it to be a static method.
+   */
+  static createServer() {
+    if (!this.app) {
+      this.app = express();
+      this.server = http.Server(this.app);
+
+      this.server.listen(8000);
+      console.log('Listening at http://localhost:8000');
+    }
   }
 }
 
 // Make instance of Server object
-const chatApp = new Server();
+Server.createServer();
