60054fa950b4960c13389372e4056078415ef9b2
cav2008 <cavanlaw@hotmail.com>
2017-09-24 13:33:36 +0100

    add comments and refactor google scraper component constructor

--- src/server/components/google-scraper/google-scraper.js
+++ src/server/components/google-scraper/google-scraper.js
@@ -4,7 +4,16 @@ import request from 'request';
 import cheerio from 'cheerio';
 
 export default class GoogleScraper {
+  // constructors should not have any methods, only call them.
   constructor() {
+    this.html = '';
+    this.scrapeData();
+  }
+
+  /**
+   * Go to www.google.com and grabs the html data.
+   */
+  scrapeData() {
     request('http://www.google.com', (error, response, responseHtml) => {
       let html = responseHtml;
       this.html = html;
